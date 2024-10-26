cbde5250ca830a4a381a6be383011f3de8f6bd3c
Shivam verma <shivamverma2001sj@gmail.com>
2024-10-26 15:23:19 +0530

    project setUp and app configurations

---
+++ SvBlog/src/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    appWriteURL : String(import.meta.VITE_APPWRITE_URL),
+    appWriteProjectId : String(import.meta.VITE_APPWRITE_PROJECT_ID),
+    appWriteDatabaseId : String(import.meta.VITE_APPWRITE_DATABASE_ID),
+    appWriteCollectinoId : String(import.meta.VITE_APPWRITE_COLLECTION_ID),
+    appWriteBucketId : String(import.meta.VITE_APPWRITE_BUCKTED_ID)
+}
+
+export default config;
