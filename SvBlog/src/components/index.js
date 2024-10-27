1298e6869bc7633dd19a5d69a0ee9e7024191819
Shivam verma <shivamverma2001sj@gmail.com>
2024-10-27 11:37:50 +0530

    components

--- SvBlog/src/components/index.js
+++ SvBlog/src/components/index.js
@@ -1,4 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from './container/Container';
+import Logo from "./Logo/Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button/Button";
 
-export {Header, Footer}
+export {Header, Footer, Container, Logo, LogoutBtn, Button};
